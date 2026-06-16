7de9f11df6c40af89c258ee6844550cec0f8eeda
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-06-16 21:41:25 +0530

    feat: add login logic with JWT authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,3 +39,48 @@ const register = async (req, res) => {
     }
 }
 
+// ================= LOGIN USER =================
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // 1. Check user exists
+        const user = await User.findOne({ email }).select("+password");
+        
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        // 2. Check password
+        const isMatch = await user.comparePassword(password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        // 3. Status online
+        user.status = "online";
+        user.lastSeen = null;
+        await user.save();
+
+        // 3. Generate JWT
+        const token = generateToken(user._id);
+
+        // 4. Send Cookie
+        res.cookie("token", token, cookieOptions);
+
+        // 5. Send response
+        res.json({
+            message: "Login successful",
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                status: user.status
+            },
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
