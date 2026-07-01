958a0be6998f4de8106a81379edb85ff649559fe
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-01 18:42:59 +0530

    refactor(auth): standardize JWT payload to use id instead of userId

--- models/User.js
+++ models/User.js
@@ -70,12 +70,11 @@ const userSchema = new mongoose.Schema(
 );
 
 // 🔐 HASH PASSWORD
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next(); // 🔥 FIX (missing next)
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return; 
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // 🔑 COMPARE PASSWORD
