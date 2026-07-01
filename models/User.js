3d68736049029595730cf7f38cbbd77cdee98b73
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-01 21:12:09 +0530

    feat(models): add User schema with validation, bcrypt hashing, and auth methods

---
+++ models/User.js
@@ -0,0 +1,82 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+      trim: true,
+      minlength: 3,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minlength: 6,
+      select: false,
+      match: [
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{6,}$/,
+        "Password must contain at least one number and one uppercase and lowercase letter, and at least 6 or more characters",
+      ],
+    },
+
+    phone: {
+      type: String,
+      trim: true,
+      match: [/^[0-9]{10}$/, "Please add a valid phone number"],
+    },
+
+    wishlist: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Product",
+    },
+
+    // 🛒 Cart (Cars user is interested in buying / booking)
+    cart: [
+      {
+        car: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Car",
+        },
+        addedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+
+    role: {
+      type: String,
+      enum: ["user", "admin", "seller"],
+      default: "user",
+    },
+  },
+
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
