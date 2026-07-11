4048ea9fe51cd3d2a1e155c4155aca405069aac6
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-11 22:48:02 +0530

    feat(user): add routes for admin to manage dealers with role-based access

---
+++ routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const { createDealer } = require("../controllers/userController");
+const { protect } = require("../middlewares/authMiddleware");
+const { restrictTo } = require("../middlewares/roleMiddleware");
+
+// 🔥 Admin creates dealer
+router.post("/create-dealer", protect, restrictTo("admin"), createDealer);
+
+module.exports = router;
