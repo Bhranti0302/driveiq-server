0a57818cf85a1a2bdcb5a71056d9829869297d24
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-11 18:47:26 +0530

    refactor(auth): add header token support, handle jwt errors, and exclude password from user

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,29 +3,46 @@ const User = require("../models/User");
 const asyncHandler = require("./../utils/asyncHandler");
 
 const protect = asyncHandler(async (req, res, next) => { 
-    const token = req.cookies.token;
+    const token;
 
-    // 1. Check if token exists
+    // 1. Get token from header or cookie
+    if (req.cookies.token) {
+        token = req.cookies.token;
+    } else if(req.headers.authorization?.startsWith("Bearer ")) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+    // 2. Check if token exists
     if (!token) {
         res.status(401).json({
             message: "Not authorized, no token"
         })
     }
 
-    // 2. Verify token
-    const decodedId = jwt.verify(token, process.env.JWT_SECRET);
+    try {
+        // 3. Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // 3. Check if user exists
-    const user = await User.findById(decodedId.userId);
+        // 4. Get user
+        const user = await User.findById(decoded.userId).select("-password");
 
-    if (!user) {
+        // 5. Check if user exists
+        if (!user) {
+            res.status(401).json({
+                message: "Not authorized, user not found"
+            })
+        }
+
+        // 6. Attach user
+        req.user = user;
+
+        next();
+        
+    }catch (error) {
         res.status(401).json({
-            message: "Not authorized, user not found"
+            message: "Not authorized, token failed"
         })
     }
 
-    req.user = user;
-    next();
 })
 
 module.exports = { protect };
