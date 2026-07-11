810d067eaec560b3bbacda55f2ae77ebf479b7f8
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-11 22:47:15 +0530

    refactor(auth): improve auth middleware with JWT validation and user attachment

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,48 +2,45 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const asyncHandler = require("./../utils/asyncHandler");
 
-const protect = asyncHandler(async (req, res, next) => { 
-    let token;
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
 
-    // 1. Get token from header or cookie
-    if (req.cookies.token) {
-        token = req.cookies.token;
-    } else if(req.headers.authorization?.startsWith("Bearer ")) {
-        token = req.headers.authorization.split(" ")[1];
-    }
-    // 2. Check if token exists
-    if (!token) {
-        res.status(401).json({
-            message: "Not authorized, no token"
-        })
-    }
+  // 1. Get token
+  if (req.cookies.token) {
+    token = req.cookies.token;
+  } else if (req.headers.authorization?.startsWith("Bearer ")) {
+    token = req.headers.authorization.split(" ")[1];
+  }
 
-    try {
-        // 3. Verify token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  // 2. No token
+  if (!token) {
+    return res.status(401).json({
+      message: "Not authorized, no token",
+    });
+  }
 
-        // 4. Get user
-        const user = await User.findById(decoded.userId).select("-password");
+  try {
+    // 3. Verify
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        // 5. Check if user exists
-        if (!user) {
-            res.status(401).json({
-                message: "Not authorized, user not found"
-            })
-        }
+    // 4. Get user
+    const user = await User.findById(decoded.userId).select("-password");
 
-        // 6. Attach user
-        req.user = user;
-
-        next();
-        
-    }catch (error) {
-        res.status(401).json({
-            message: "Not authorized, token failed"
-        })
+    if (!user) {
+      return res.status(401).json({
+        message: "Not authorized, user not found",
+      });
     }
 
-})
+    // 5. Attach user
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Not authorized, token failed",
+    });
+  }
+});
 
 module.exports = { protect };
-    
