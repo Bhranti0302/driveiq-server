3e80158973b74eb7a736abeabf9249f543385f9a
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-13 22:42:39 +0530

    feat(routes): add product routes for dealer creation and admin approval/rejection

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,14 +6,22 @@ const { restrictTo } = require("../middlewares/roleMiddleware");
 const {
   createProduct,
   getAllProducts,
+  approveProduct,
+  rejectProduct,
 } = require("../controllers/productController");
 
 const router = express.Router();
 
-// 🏪 Dealer
+// 🏪 Dealer → Create product
 router.post("/", protect, restrictTo("dealer"), createProduct);
 
-// 👤 All logged users
+// 👤 Logged users → View products
 router.get("/", protect, getAllProducts);
+
+// 👑 Admin → Approve
+router.put("/:id/approve", protect, restrictTo("admin"), approveProduct);
+
+// 👑 Admin → Reject
+router.put("/:id/reject", protect, restrictTo("admin"), rejectProduct);
 
 module.exports = router;
