98bc066e7447a1a97392225380f7623ed8d5dcf1
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-13 21:48:55 +0530

    feat(routes): add protected route for fetching all products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,10 +3,17 @@ const express = require("express");
 const { protect } = require("../middlewares/authMiddleware");
 const { restrictTo } = require("../middlewares/roleMiddleware");
 
-const { createProduct } = require("../controllers/productController");
+const {
+  createProduct,
+  getAllProducts,
+} = require("../controllers/productController");
 
 const router = express.Router();
 
+// 🏪 Dealer
 router.post("/", protect, restrictTo("dealer"), createProduct);
+
+// 👤 All logged users
+router.get("/", protect, getAllProducts);
 
 module.exports = router;
