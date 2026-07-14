5ded7ac88318747fa9b4a4f19c47004475710a66
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-14 22:38:56 +0530

    feat(user-routes): add protected route for user profile update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { createDealer } = require("../controllers/userController");
+const { createDealer, updateUser } = require("../controllers/userController");
 const { protect } = require("../middlewares/authMiddleware");
 const { restrictTo } = require("../middlewares/roleMiddleware");
 
 // 🔥 Admin creates dealer
 router.post("/create-dealer", protect, restrictTo("admin"), createDealer);
+
+// Dealer + Admin can update
+router.put("/profile", protect, restrictTo("admin", "dealer"), updateUser);
 
 module.exports = router;
