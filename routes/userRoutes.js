bca070422d4c7f65d6f5334f5cf93cba9dc8f8a0
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-15 18:04:53 +0530

    fix(user): allow only logged-in user to access profile update route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { createDealer, updateUser } = require("../controllers/userController");
+const { createDealer, updateUserProfile } = require("../controllers/userController");
 const { protect } = require("../middlewares/authMiddleware");
 const { restrictTo } = require("../middlewares/roleMiddleware");
 
 // 🔥 Admin creates dealer
 router.post("/create-dealer", protect, restrictTo("admin"), createDealer);
 
-// Dealer + Admin can update
-router.put("/profile", protect, restrictTo("admin", "dealer"), updateUser);
+// User updates profile
+router.put("/profile", protect, updateUserProfile);
 
 module.exports = router;
