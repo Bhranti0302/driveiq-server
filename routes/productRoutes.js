1dd561198c62b75e42bf1cec4788c348308dfe60
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-20 14:20:37 +0530

    docs(product-routes): refine comments and improve readability

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -14,22 +14,22 @@ const {
 
 const router = express.Router();
 
-// 🏪 Dealer → Create product
+// Role => Dealer → Create product
 router.post("/", protect, restrictTo("dealer"), createProduct);
 
-// 👤 Logged users → Vi ew products
+//Logged users → View products as per role
 router.get("/", protect, getAllProducts);
 
-// 👑 Admin → Approve
+// Admin → Approve product 
 router.put("/:id/approve", protect, restrictTo("admin"), approveProduct);
 
-// 👑 Admin → Reject
+// 👑 Admin → Reject product
 router.put("/:id/reject", protect, restrictTo("admin"), rejectProduct);
 
-// Dealer + Admin can update
+// Dealer + Admin can update product
 router.put("/:id", protect, restrictTo("admin", "dealer"), updateProduct);
 
-// Dealer + Admin can delete
+// Dealer + Admin can delete product
 router.delete("/:id", protect, restrictTo("admin", "dealer"), deleteProduct);
 
 module.exports = router;
