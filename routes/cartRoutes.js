5c6491dc61d7781e14020e2cbb2eed7a87895094
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-20 16:20:08 +0530

    feat: add route for getUserCart endpoint with authentication

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const router = express.Router();
 
-const { addToCart } = require("../controllers/cartController");
+const { addToCart, getUserCart } = require("../controllers/cartController");
 
 const { protect } = require("../middlewares/authMiddleware");
 
 // Add to cart
 router.post("/", protect, addToCart);
+
+// Get Cart
+router.get("/", protect, getUserCart);
 
 module.exports = router
