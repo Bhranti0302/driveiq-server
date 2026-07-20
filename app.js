94e533f097e837688871760303fbd5d0ab3922d0
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-20 16:08:42 +0530

    feat: integrate cart routes into app configuration

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoutes);
 
 module.exports = app;
 
