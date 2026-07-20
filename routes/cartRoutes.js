c1755349fbfe6bf623cc0db62eb2f6dd7ed2b6c2
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-20 17:23:08 +0530

    feat: add route for updateCartItem endpoint with protected access

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router();
 
-const { addToCart, getUserCart } = require("../controllers/cartController");
+const { addToCart, getUserCart, updateCartItem } = require("../controllers/cartController");
 
 const { protect } = require("../middlewares/authMiddleware");
 
@@ -10,5 +10,8 @@ router.post("/", protect, addToCart);
 
 // Get Cart
 router.get("/", protect, getUserCart);
+
+// Update Cart
+router.put("/:productId", protect, updateCartItem);
 
 module.exports = router
