b52126f54648e00fe9dc8e771f9af478da8e162e
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-07-20 14:20:26 +0530

    docs(auth-routes): update route comments for clarity

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,12 +8,18 @@ const router = express.Router();
 
 // =============== PUBLIC ROUTES ===============
 
+// Role => User Register
 router.post("/register", register);
+
+// Role => User Login
 router.post("/login", login);
 
 // =============== PROTECTED ROUTES ===============
 
+// Role => All Role logout 
 router.get("/logout", logout);
+
+// Role => All Role get user details
 router.get("/me", protect, (req, res) => {
     res.status(200).json(req.user);
 });
