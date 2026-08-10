54e00acdb7a674b94416db228b4af4157f2904f7
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-08-10 22:41:45 +0530

    refactor: integrate logging and error handling middleware in app

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const morgan = require("morgan"); // ✅ fixed
+const logger = require("./middleware/logger");
+const errorLogger = require("./middleware/errorLogger"); // ✅ add
+const errorHandler = require("./middleware/errorHandler"); // ✅ add
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -11,16 +15,21 @@ const wishlistRoutes = require("./routes/wishlistRoutes");
 
 const app = express();
 
+// 🔹 Middlewares
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173", 
     credentials: true,
   }),
 );
 
+app.use(morgan("dev"));
+app.use(logger);
+
+// 🔹 Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
@@ -28,5 +37,8 @@ app.use("/api/carts", cartRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 
-module.exports = app;
+// ❗ ERROR MIDDLEWARE (VERY IMPORTANT ORDER)
+app.use(errorLogger); // logs error
+app.use(errorHandler); // sends response
 
+module.exports = app;
